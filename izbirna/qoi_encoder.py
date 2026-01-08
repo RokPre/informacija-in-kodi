96f189766c9b37bbb477786f5108534e55eac951
Rok Prezelj <prezelj.rok@gmail.com>
2026-01-08 18:25:47 +0100

    Updated the encode to support RGBA

--- izbirna/qoi_encoder.py
+++ izbirna/qoi_encoder.py
@@ -6,62 +6,142 @@ import cv2
 import sys
 import os
 
-image_name = sys.argv[1]
+SUPPORTED_FILE_TYPES = ["png"]
 
-# Check if file is valid
-if not os.path.isfile(image_name):
-    raise OSError("File is not valid or does not exist.")
 
-image = cv2.imread(image_name)
+def encode_RGBA(image, height, width):
+    output_bytes = bytearray()
+    prev_pixel: list[int] = [0, 0, 0, 255]
+    running_list = [[0, 0, 0, 0] for _ in range(64)]
+    for h in range(height):
+        for w in range(width):
+            pixel = image[h][w]
+            pixel = [int(chanel) for chanel in pixel]
 
-height = image.shape[0]
-width = image.shape[1]
+            color_hash = (pixel[2] * 3 + pixel[1] * 5 + pixel[0] * 7 + pixel[3] * 11) % 64
 
-output_bytes: bytearray = bytearray()
-prev_pixel: list[int] = [0, 0, 0]
-running_list = [[0, 0, 0] for _ in range(64)]
-last_index = -1
+            if pixel == running_list[color_hash] and pixel != prev_pixel:
+                output_bytes.append(color_hash)
+                prev_pixel = pixel.copy()
+                running_list[color_hash] = pixel
+                continue
 
-# Header
-output_bytes.extend("qoif".encode("ascii"))
-output_bytes.extend(width.to_bytes(length=4, byteorder="big"))  # Width
-output_bytes.extend(height.to_bytes(length=4, byteorder="big"))  # Width
-output_bytes.append(0b00000011)  # Chanels
-output_bytes.append(0b00000001)  # Colorspace
+            # If colors are similar only store the differences
+            # Check the first 6 bits fo the current pixels color chanels and the previous pixels color chanels
+            diff = [pixel[i] - prev_pixel[i] for i in range(len(pixel))]
+            if -2 <= diff[0] <= 1 and -2 <= diff[1] <= 1 and -2 <= diff[2] <= 1 and diff[3] == 0:
+                output_bytes.append(0b01000000 | ((diff[2] + 2) << 4) | ((diff[1] + 2) << 2) | (diff[0] + 2))
+                prev_pixel = pixel.copy()
+                running_list[color_hash] = pixel
+                continue
 
-# Data
-for h in range(height):
-    for w in range(width):
-        pixel = image[h][w][::-1]
-        pixel = [int(color) for color in pixel]
+            output_bytes.extend([int(0b11111111), pixel[2], pixel[1], pixel[0], pixel[3]])
 
-        color_hash = (pixel[0] * 3 + pixel[1] * 5 + pixel[2] * 7) % 64
-
-        if pixel == running_list[color_hash] and pixel != prev_pixel:
-            output_bytes.append(color_hash)
-            prev_pixel = pixel.copy()
+            # Update the running_.ist with the current pixel
             running_list[color_hash] = pixel
-            continue
-
-        # If colors are similar only store the differences
-        # Check the first 6 bits fo the current pixels color chanels and the previous pixels color chanels
-        dr = pixel[0] - prev_pixel[0]
-        dg = pixel[1] - prev_pixel[1]
-        db = pixel[2] - prev_pixel[2]
-        if -2 <= dr <= 1 and -2 <= dg <= 1 and -2 <= db <= 1:
-            output_bytes.append(0b01000000 | ((dr + 2) << 4) | ((dg + 2) << 2) | (db + 2))
             prev_pixel = pixel.copy()
+
+    return output_bytes
+
+
+def encode_RGB(image, height, width):
+    output_bytes = bytearray()
+    prev_pixel: list[int] = [0, 0, 0]
+    running_list = [[0, 0, 0] for _ in range(64)]
+    for h in range(height):
+        for w in range(width):
+            pixel = image[h][w]
+            pixel = [int(chanel) for chanel in pixel]
+
+            color_hash = (pixel[2] * 3 + pixel[1] * 5 + pixel[0] * 7) % 64
+
+            if pixel == running_list[color_hash] and pixel != prev_pixel:
+                output_bytes.append(color_hash)
+                prev_pixel = pixel.copy()
+                running_list[color_hash] = pixel
+                continue
+
+            # If colors are similar only store the differences
+            # Check the first 6 bits fo the current pixels color chanels and the previous pixels color chanels
+            diff = [pixel[i] - prev_pixel[i] for i in range(len(pixel))]
+            if -2 <= diff[0] <= 1 and -2 <= diff[1] <= 1 and -2 <= diff[2] <= 1:
+                output_bytes.append(0b01000000 | ((diff[2] + 2) << 4) | ((diff[1] + 2) << 2) | (diff[0] + 2))
+                prev_pixel = pixel.copy()
+                running_list[color_hash] = pixel
+                continue
+
+            output_bytes.extend([int(0b11111110), pixel[2], pixel[1], pixel[0]])
+
+            # Update the running_.ist with the current pixel
             running_list[color_hash] = pixel
-            continue
+            prev_pixel = pixel.copy()
+    return output_bytes
 
-        output_bytes.extend([int(0b11111110), pixel[0], pixel[1], pixel[2]])
 
-        # Update the running_.ist with the current pixel
-        running_list[color_hash] = pixel
-        prev_pixel = pixel.copy()
+def main():
+    # Check if user inputed a file
+    if len(sys.argv) < 2:
+        raise ValueError("No input file specified.")
 
-# Ending mark
-output_bytes.extend(b"\x00\x00\x00\x00\x00\x00\x00\x01")
+    if len(sys.argv) == 2:
+        args = [sys.argv[1]]
+    else:
+        args = sys.argv[1:]
 
-with open(image_name.replace("png", "qoi"), "wb") as f:
-    f.write(output_bytes)
+    for image_name in args:
+        # Check if file is valid
+        if not os.path.isfile(image_name):
+            raise OSError(f"File is not valid or does not exist: {image_name}")
+
+        try:
+            file_type = image_name.split(".")[-1]
+        except:
+            raise ValueError("Unable to determine the file type.")
+
+        if file_type.lower() not in SUPPORTED_FILE_TYPES:
+            raise NotImplementedError("File type is not supported:", file_type)
+
+        image = cv2.imread(image_name, cv2.IMREAD_UNCHANGED)
+
+        height = image.shape[0]
+        width = image.shape[1]
+        is_RGBA = image.shape[2] == 4
+
+        # Init of output array
+        output_bytes: bytearray = bytearray()
+
+        # Header
+        output_bytes.extend("qoif".encode("ascii"))  # File signature
+        output_bytes.extend(width.to_bytes(length=4, byteorder="big"))  # Width
+        output_bytes.extend(height.to_bytes(length=4, byteorder="big"))  # Height
+
+        if is_RGBA:
+            output_bytes.append(0b00000100)  # Chanels
+        else:
+            output_bytes.append(0b00000011)  # Chanels
+
+        is_SRGB: bool = False  # Typically false. TODO: Detection
+        if is_SRGB:
+            output_bytes.append(0b00000000)  # Colorspace
+        else:
+            output_bytes.append(0b00000001)  # Colorspace
+
+        if is_RGBA:
+            data = encode_RGBA(image, height, width)
+        else:
+            data = encode_RGB(image, height, width)
+
+        output_bytes.extend(data)
+
+        # Ending mark
+        output_bytes.extend(b"\x00\x00\x00\x00\x00\x00\x00\x01")
+
+        output_file_name = image_name.replace(file_type, "qoi")
+        with open(output_file_name, "wb") as f:
+            f.write(output_bytes)
+
+        print(f"File saved in: {output_file_name}")
+
+
+if __name__ == "__main__":
+    main()
